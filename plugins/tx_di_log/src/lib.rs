7ad371c3f23f78ae322f47629b3c4c672c19704a
tx <845360351@qq.com>
2026-05-09 11:17:12 +0800

    refactor(tx_di_log): 重构模块导出顺序
    - 调整 pub use 语句的顺序，将 config 模块的导出移到末尾
    - 移除测试模块中的冗余代码，保留核心功能实现
    - 优化模块结构以提高代码可读性

--- plugins/tx_di_log/src/lib.rs
+++ plugins/tx_di_log/src/lib.rs
@@ -3,21 +3,3 @@ mod comp;
 
 pub use comp::*;
 pub use config::*;
-
-#[cfg(test)]
-mod tests {
-    use log::{debug, error, info, warn};
-    use tx_di_core::BuildContext;
-
-    #[test]
-    fn it_works() {
-        // Some(r"D:\proj\tx_di\configs\test_log.toml")
-        //
-        let _ctx = BuildContext::new(Some(r"D:\proj\tx_di\configs\test_log.toml"));
-        BuildContext::debug_registry().expect("TODO: panic message");
-        debug!("测试日志框架加载");
-        info!("测试日志框架加载");
-        warn!("测试日志框架加载");
-        error!("测试日志框架加载");
-    }
-}
