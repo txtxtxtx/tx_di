7448b80cadd23f0ccb14c443a028308b16da4e3c
tx <845360351@qq.com>
2026-05-26 14:01:23 +0800

    chore(config): 更新配置文件路径
    - 将配置文件路径从 C 盘路径更新为 D 盘路径
    - 保留原始路径作为注释以便参考

--- examples/gb28181_admin/src/main.rs
+++ examples/gb28181_admin/src/main.rs
@@ -33,7 +33,8 @@ use tx_di_log;
 async fn main() -> anyhow::Result<()> {
     // examples/gb28181_admin/config/config.toml
     // C:\a_me\proj\rust\tx_di\examples\gb28181_admin\config\config.toml
-    let app = BuildContext::new(Some(r"C:\a_me\proj\rust\tx_di\examples\gb28181_admin\config\config.toml"))
+    // D:\proj\tx_di\examples\gb28181_admin\config\config.toml
+    let app = BuildContext::new(Some(r"D:\proj\tx_di\examples\gb28181_admin\config\config.toml"))
         .build()?
         .ins_run()
         .await?;
